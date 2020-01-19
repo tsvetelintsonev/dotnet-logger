54191108a14bd4c7a6bd83c799bf0495a90dcd8c
Tsvetelin Tsonev <github.tsonev@yahoo.com>
2020-01-19 22:51:41 +0100

    Add comments to logger

--- Solution/Logger.cs
+++ Solution/Logger.cs
@@ -5,15 +5,23 @@ using System.Threading.Tasks;
 
 namespace Solution
 {
+    /// <summary>
+    /// The core logging pipeline.
+    /// </summary>
     public class Logger : ILogger
     {
         private readonly IList<ISink> _sinks;
 
+        /// <summary>
+        /// Constructs a new <see cref="Logger"/>
+        /// </summary>
+        /// <param name="sinks">The sinks to which the log statements will be written to.</param>
         public Logger(IList<ISink> sinks)
         {
             _sinks = sinks ?? throw new ArgumentNullException(nameof(sinks));
         }
-        
+
+        /// <inheritdoc />
         public void Log(string message, LogLevel? logLevel = null)
         {
             Task.Run(() =>
@@ -27,29 +35,34 @@ namespace Solution
                 {
                     // Send email etc.
                 }
-            }).Wait();
+            });
         }
 
+        /// <inheritdoc />
         public void LogDebug(string message) 
         {
             Log(message, LogLevel.Debug);
         }
 
+        /// <inheritdoc />
         public void LogInformation(string message)
         {
             Log(message, LogLevel.Information);
         }
 
+        /// <inheritdoc />
         public void LogWarning(string message)
         {
             Log(message, LogLevel.Warning);
         }
 
+        /// <inheritdoc />
         public void LogError(string message)
         {
             Log(message, LogLevel.Error);
         }
 
+        /// <inheritdoc />
         public ILogStatement CurrentLogStatement { get; private set; }
 
         private void WriteToAllSinks(string message) 
