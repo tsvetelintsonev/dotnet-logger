c596123ffb4ab6c96e1757d20aaa2fe20a74ad4e
Tsvetelin Tsonev <github.tsonev@yahoo.com>
2020-01-19 20:48:39 +0100

    LogStatement initial implementation

---
+++ Solution/ILogStatement.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Solution
+{
+    public interface ILogStatement
+    {
+        DateTimeOffset Timestamp { get; }
+        LogLevel LogLevel { get; }
+        string Message { get; }
+    }
+}
---
+++ Solution/LogStatement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Solution
+{
+    /// <summary>
+    /// A log statement
+    /// </summary>
+    public class LogStatement : ILogStatement
+    {
+        /// <summary>
+        /// Constructs a new <see cref="LogStatement"/>
+        /// </summary>
+        /// <param name="timestamp">The timestamp at which the log statement occurred.</param>
+        /// <param name="logLevel">The level of the log statement.</param>
+        /// <param name="message">The message describing the log statement.</param>
+        public LogStatement(DateTimeOffset timestamp, LogLevel logLevel, string message)
+        {
+            Timestamp = timestamp;
+            LogLevel = logLevel;
+            Message = message ?? throw new ArgumentNullException(nameof(message));
+        }
+
+        public DateTimeOffset Timestamp { get; private set; }
+
+        public LogLevel LogLevel { get; private set; }
+
+        public string Message { get; private set; }
+
+        public override string ToString() 
+        {
+            return $"[{Timestamp.ToString("yyyy-MM-dd")}] [{LogLevel}] [{Message}]";
+        }
+    }
+}
